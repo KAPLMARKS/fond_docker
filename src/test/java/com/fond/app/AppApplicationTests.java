c521276f93809ff8cf3b61fc79301cc61486fa7e
MIShigabutdinov <MIShigabutdinov@kai.ru>
2022-11-14 16:21:16 +0300

    docker

---
+++ src/test/java/com/fond/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fond.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
